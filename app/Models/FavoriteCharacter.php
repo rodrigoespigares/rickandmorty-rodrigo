0551a69521e1768622c1238f50a045cc07a24e6b
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-08-20 10:37:35 +0200

    Cambios sobre el modelo de FavoriteCharacter para ocultar datos innecesarios

--- app/Models/FavoriteCharacter.php
+++ app/Models/FavoriteCharacter.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class FavoriteCharacter extends Model
 {
     use HasFactory;
+
+    protected $table = 'favorite_characters';
+
+    protected $fillable = [
+        'user_id',
+        'character_id',
+    ];
+
+    protected $hidden = [
+        'id',
+        'user_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
