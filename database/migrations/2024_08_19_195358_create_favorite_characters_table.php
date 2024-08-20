b87bcd6cfd68ac42b18737ce35eafa313d96d094
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-08-20 09:12:34 +0200

    Created FavoriteCharacter model and generated migration for database schema update.

---
+++ database/migrations/2024_08_19_195358_create_favorite_characters_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private const TABLE_NAME = 'favorite_characters';
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(self::TABLE_NAME, function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('character_id'); // ID del personaje de la API de Rick & Morty
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE_NAME);
+    }
+};
