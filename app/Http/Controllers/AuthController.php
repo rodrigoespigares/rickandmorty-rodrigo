3ea0d4f7bfb06542dd5194d15e7d80f03d4bd8cd
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-08-20 09:51:38 +0200

    Cambios sobre el login y registro y la configuración para la carga de autentificación mediante la librería laravel/passport y laravel/sanctum

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,33 +8,37 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
+    public function register(Request $request){
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
         ]);
-    
+
         $user = User::create([
             'name' => $validated['name'],
             'email' => $validated['email'],
             'password' => bcrypt($validated['password']),
         ]);
-    
-        return response()->json(['token' => $user->createToken('API Token')->plainTextToken]);
+        
+        $token = $user->createToken('API Token')->accessToken;
+
+        return response()->json(['token' => $token], 201);
     }
-    
-    public function login(Request $request) {
+
+    public function login(Request $request){
         $credentials = $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
         ]);
-    
+
         if (!Auth::attempt($credentials)) {
             return response()->json(['message' => 'Invalid login details'], 401);
         }
-    
+
         $user = $request->user();
-        return response()->json(['token' => $user->createToken('API Token')->plainTextToken]);
+        $token = $user->createToken('API Token')->accessToken;
+
+        return response()->json(['token' => $token], 200);
     }
 }
