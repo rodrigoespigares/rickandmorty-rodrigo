79df4ac7bea7fe58f6929b303a1af8468776866d
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-08-20 09:14:39 +0200

    Añadido fichero de rutas de API

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\FavoriteCharacterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+|
+*/
+
+Route::get('characters', [CharacterController::class, 'index']);
+Route::get('characters/{id}', [CharacterController::class, 'show']);
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::post('favorites', [FavoriteCharacterController::class, 'store']);
+    Route::get('favorites', [FavoriteCharacterController::class, 'index']);
+    Route::delete('favorites/{id}', [FavoriteCharacterController::class, 'destroy']);
+});
