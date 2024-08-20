1f10e9ffdbdd6412ea7498c955baf85809ba735c
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-08-20 12:30:59 +0200

    Filtro de busqueda para listado de personajes

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -3,18 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 
 class CharacterController extends Controller
 {
     public function index(Request $request) {
         
         $page = $request->query('page', 1);
+        $name = $request->query('name', "");
+        $species = $request->query('species', "");
+        $gender = $request->query('gender', "");
+        $type = $request->query('type', "");
 
-        $characters = (new \App\Services\RickAndMortyService)->getCharacters(["page" => $page]);
-        if(!$characters['results']){
+        $params = [];
+        $page? $params['page'] = $page : null;
+        $name? $params['name'] = $name : null;
+        $species? $params['species'] = $species : null;
+        $gender? $params['gender'] = $gender : null;
+        $type? $params['type'] = $type : null;
+
+        $characters = (new \App\Services\RickAndMortyService)->getCharacters($params);
+
+        if(!isset($characters['info'])){
             return response()->json(['error' => true, "message" => "No characters found"], 404);
         }
-        return response()->json($characters['results'], 200);
+        $characters['info']['next'] = $page + 1 >= $characters['info']['pages'] ? null : URL::route('characters.index', ['page' => $page + 1]);
+        $characters['info']['prev'] = $page - 1 <= 0 ? null : URL::route('characters.index', ['page' => $page - 1]);
+
+        return response()->json($characters, 200);
     }
 
     public function show($id) {
